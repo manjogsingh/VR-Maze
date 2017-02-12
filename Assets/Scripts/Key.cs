f5b21ecae4038c0d26bd6ffec2d80fc85106c1f3
manjogsingh <manjogsingh@gmail.com>
2017-02-12 17:19:16 +0530

    Maze Setup
    Location of all objects done.

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour 
+{
+    //Create a reference to the KeyPoofPrefab and Door
+    //Create a variable to store whether or not the key has been collected or not
+
+	void Update()
+	{
+		//Not required, but for fun why not try adding a Key Animation here :)
+	}
+
+	public void OnKeyClicked()
+	{
+        // Instatiate the KeyPoof Prefab where this key is located
+        // Make sure the poof animates vertically
+        // Call the Unlock() method on the Door
+        // Set the Key Collected Variable to true
+        // Destroy the key. Check the Unity documentation on how to use Destroy
+    }
+
+}
---
+++ Assets/Scripts/ObjectRotation.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotation : MonoBehaviour {
+	void Update () {
+		transform.RotateAround(transform.position,new Vector3(0,1.0f,0),5.0f);
+	}
+}
